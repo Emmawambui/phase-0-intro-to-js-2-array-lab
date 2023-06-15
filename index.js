e064a6dd3e83d08d94817fe7b78ae23bb834b3c2
emma wambui <emma.wambui@student.moringaschool.com>
2023-06-15 19:44:16 +0300

    partially done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph){
+    cats.push('Ralph');
+ }
+function destructivelyPrependCat(Bob){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop('Garfied')
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift("Milo")
+}
+
+function appendCat(Broom) {
+    const newArray = [...cats, Broom];
+    return newArray;
+  }
+
+function prependCat(Garfield){
+    cats.unshift(arrayofcats,1,2)
+}
+function removeFirstCat(Garfield){
+    cats.shift()
+}
